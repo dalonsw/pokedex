5f7cdbb8c39152525bc549989b8c51866666041f
Daniel <danielalonso.os@gmail.com>
2024-10-21 16:35:48 -0300

    API de tradução para a descrição do Pokémon

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,9 @@ const pSpecies = document.getElementById("poke-species");
 const pNumber = document.getElementById("poke-num");
 const pImage = document.getElementById("poke-img");
 const pTypes = document.getElementsByClassName("types");
-
+const pWeight = document.getElementById("peso");
+const pHeight = document.getElementById("altura");
+const pDescription = document.getElementById("pokedex-description");
 
 //Pegar informações sobre a espécie Pokémon
 const fetchPokemonSpecies = async (pokemon) => {
@@ -24,23 +26,20 @@ const fetchPokemon = async (pokemon) => {
     return data
 }
 
+//API de Tradução
+const translate = async (text) => {
+    const APIResponse = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=en|pt`)
+    const data = await APIResponse.json()
+    return data
+} 
+
 //Função para atualizar e mostrar dados no Front-end
 const mostrarInformacoesPokemonSpecies = async (data) => {
     //Pegar mais informações
     const pokemonMoreData = await fetchPokemon(data.id)
-    await console.log(pokemonMoreData.types)
+    console.log(pokemonMoreData)
 
-    //Alterar e tratar os dados
-    const pokeNumber = `${data.id}`;
-    const imgURL = `https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/${pokeNumber.padStart(3, "0")}.png`
-    const formatarNome = (nome) => {
-        let nomeArray = nome.split("");
-        let primeiraLetraUpper = nomeArray.shift().toUpperCase();
-        nomeArray.unshift(primeiraLetraUpper);
-        let nomeCompleto = nomeArray.join("")
-        return nomeCompleto;
-    }
-
+//Alterar e tratar os dados
     //Validar o título em inglês
     const validarGenus = (generaArray) => {
         for(let i = 0; i < generaArray.length; i++){
@@ -50,17 +49,79 @@ const mostrarInformacoesPokemonSpecies = async (data) => {
         }
     }
 
+    //Validar o descrição em inglês
+    const validarDesc = (descArray) => {
+        for(let i = 0; i < descArray.length; i++){
+            if(data.flavor_text_entries[i].language.name === "en"){
+                return data.flavor_text_entries[i].flavor_text
+            }
+        }
+    }
+        
+    const pokeNumber = `${data.id}`;
+    const imgURL = `https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/${pokeNumber.padStart(3, "0")}.png`
+    const formatarNome = (nome) => {
+        let nomeArray = nome.split("");
+        let primeiraLetraUpper = nomeArray.shift().toUpperCase();
+        nomeArray.unshift(primeiraLetraUpper);
+        let nomeCompleto = nomeArray.join("")
+        return nomeCompleto;
+    }
+    const pokeTypes = pokemonMoreData.types
+    const descricaoTraduzida = await translate(validarDesc(data.flavor_text_entries).replaceAll("\n", " ").replaceAll("\f", " "))
+
     //Atribuir dados
-    pName.innerText = formatarNome(data.name)
+    pName.innerText = formatarNome(data.name);
     pSpecies.innerText = validarGenus(data.genera);
-    pNumber.innerText = `#${pokeNumber.padStart(4, "0")}`
-    pImage.setAttribute("src", imgURL)
+    pNumber.innerText = `#${pokeNumber.padStart(4, "0")}`;
+    pImage.setAttribute("src", imgURL);
+    pWeight.innerText = pokemonMoreData.weight / 10
+    pHeight.innerText = pokemonMoreData.height / 10
+    pDescription.innerText = descricaoTraduzida.responseData.translatedText
+    console.log(descricaoTraduzida)
+    
+    //Formatar Nome Tipo
+    const formatarTipo = (tipo) => {
+        let nomeTipoArray = tipo.split('');
+        let nomeTipoUpper = nomeTipoArray.shift().toUpperCase();
+        nomeTipoArray.unshift(nomeTipoUpper);
+        let nomeTipoCompleto = nomeTipoArray.join("")
+        return nomeTipoCompleto
+    }
+
+    //Alterar o Tipo
+    for(let type in pokeTypes){
+        pTypes[type].setAttribute("id", pokeTypes[type].type.name)
+        pTypes[type].innerText = formatarTipo(pokeTypes[type].type.name)
+        if(pokeTypes.length === 1){
+            pTypes[1].setAttribute("id", "no2type")
+            pTypes[1].innerText = "no2type"
+        }
+    }
 
 }
 
-botaoPesquisa.addEventListener("click", async () => {
-    const pokeSpeciesData = await fetchPokemonSpecies(pokemonBusca.value.toLowerCase())
-    await console.log(pokeSpeciesData)
+const buscarPokemon = async () => {
+    try{
+        const pokeSpeciesData = await fetchPokemonSpecies(pokemonBusca.value.toLowerCase())
+        await console.log(pokeSpeciesData)
+    
+        await mostrarInformacoesPokemonSpecies(pokeSpeciesData);
+    }
+    catch (err){
+        pokemonBusca.value = "";
+        pokemonBusca.setAttribute("placeholder", "Esse pokémon não existe...");
+        console.log(err)
+    }
 
-    await mostrarInformacoesPokemonSpecies(pokeSpeciesData);
+}
+
+botaoPesquisa.addEventListener("click", () => {
+    buscarPokemon()
+})
+
+pokemonBusca.addEventListener("keydown", (key) => {
+    if(key.key === "Enter"){
+        buscarPokemon()
+    }
 })
